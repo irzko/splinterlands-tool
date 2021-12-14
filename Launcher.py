ddd103ac42f9bf1ea188f9d76ee608c61ee4c092
Kha Minh Tran <tranminhkha.1003@gmail.com>
2021-12-14 14:21:07 +0700

    create Launcher

---
+++ Launcher.py
@@ -0,0 +1,39 @@
+import os, time, requests
+
+logo = '''
+    \t\t\t\t\t  ██████  ██▓███   ██▓     ██▓ ▄▄▄▄   
+    \t\t\t\t\t▒██    ▒ ▓██░  ██▒▓██▒    ▓██▒▓█████▄ 
+    \t\t\t\t\t░ ▓██▄   ▓██░ ██▓▒▒██░    ▒██▒▒██▒ ▄██
+    \t\t\t\t\t  ▒   ██▒▒██▄█▓▒ ▒▒██░    ░██░▒██░█▀  
+    \t\t\t\t\t▒██████▒▒▒██▒ ░  ░░██████▒░██░░▓█  ▀█▓
+    \t\t\t\t\t▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░░ ▒░▓  ░░▓  ░▒▓███▀▒
+    \t\t\t\t\t░ ░▒  ░ ░░▒ ░     ░ ░ ▒  ░ ▒ ░▒░▒   ░ 
+    \t\t\t\t\t░  ░  ░  ░░         ░ ░    ▒ ░ ░    ░ 
+    \t\t\t\t\t      ░               ░  ░ ░   ░      
+    \t\t\t\t\t                By tmkha            ░ 
+    '''
+
+def download_resource():
+    print('\n\n\n\n\n\n\n')
+    print(logo)
+    print('\t\t\t\t\t           Đang tải dữ liệu...')
+    response = requests.get('https://raw.githubusercontent.com/tmkha/Splint/main/splint.py')
+    if response:
+        with open('splint.py', mode="w", encoding="utf-8") as f:
+            f.write(response.text)
+            f.close()
+    else: print(response)
+
+if __name__ == "__main__":
+    os.system('title Spint Tool')
+    try:
+        download_resource()
+        from splint import main
+        main()
+    except Exception as e:
+        os.system('cls')
+        print('\n\n\n\n\n\n\n')
+        print(logo)
+        print('Không có kết nối internet!')
+        print(e)
+        os.system('pause>nul|set/p =Nhấn phím bất kì để thoát ...')
