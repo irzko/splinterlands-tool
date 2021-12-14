ef2adf432792c3c85ebce8de5a2e7180c05e0aeb
Kha Minh Tran <tranminhkha.1003@gmail.com>
2021-12-14 12:31:02 +0700

    fix bug

--- splint.py
+++ splint.py
@@ -35,6 +35,8 @@ class File:
 
 class Card:
     def __init__(self):
+        response = requests.get('https://api2.splinterlands.com/cards/get_details?')
+        self.allCards = json.loads(response.text)
         try:
             self.ownerCard = File('data/playable.json')
             self.ownerCardList = self.ownerCard.rJSon()
@@ -47,9 +49,6 @@ class Card:
             list_of_card_names.append(card['name'])
         return list_of_card_names
 
-    def getAllCards(self):
-        response = requests.get('https://api2.splinterlands.com/cards/get_details?')
-        return json.loads(response.text)
 
     def sortNames(self, list_card):
         return sorted(self.getNames(list_card))
@@ -59,7 +58,7 @@ class Card:
             typeOfListCard = self.ownerCardList
             length = len(typeOfListCard)
         elif list_card == 'all':
-            typeOfListCard = self.getAllCards()
+            typeOfListCard = self.allCards
             length = len(typeOfListCard)
         elif list_card == 'na':
             typeOfListCard = self.cardNotAvailable()
@@ -101,25 +100,22 @@ class Card:
     def cardNotAvailable(self):
         cardNA = []
         sortedNameCardList = self.sortNames(self.ownerCardList)
-        for card in self.getAllCards():
+        for card in self.allCards:
             if not (card['name'] in sortedNameCardList):
                 cardNA.append(card)
         return cardNA
 
-    '''
+
     def add(self):
-        card_added = n = ''
-        if not os.path.isfile():
-            print('Đang tải thẻ bài...')
-            response = requests.get('https://raw.githubusercontent.com/tmkha/splinterlands/master/card_details.json')
-            self.allCard.wText(response.text)
+        card_added = None
+        n = None
         while (n != 'Q'):
             os.system('cls')
-            listName = self.sortListOfName(self.CardNotAvailable())
-            cardNA = self.CardNotAvailable()
-            self.showListOfNames('na')
+            listName = self.sortNames(self.cardNotAvailable())
+            cardNA = self.cardNotAvailable()
+            self.showNames('na')
             print('_' * 112)
-            if card_added != '':
+            if card_added is not None:
                 print(f'Đã thêm "{card_added}"')
             else:
                 print()
@@ -134,20 +130,21 @@ class Card:
                     if card['name'] == listName[n]:
                         self.ownerCardList.append(card)
                         card_added = listName[n]
-                        self.ownerCard.writeJSonFile(self.ownerCardList)
+                        self.ownerCard.wJSon(self.ownerCardList)
                         break
             else:
                 print('Thông tin không hợp lệ!')
                 time.sleep(1)
 
     def delete(self):
-        n = cardDeleted = ''
+        n = None
+        cardDeleted = None
         while (n != 'Q'):
-            listName = self.sortListOfName(self.ownerCardList)
+            listName = self.sortNames(self.ownerCardList)
             os.system('cls')
-            self.showListOfNames('owner')
+            self.showNames('owner')
             print('_' * 112)
-            if cardDeleted != '':
+            if cardDeleted is not None:
                 print(f'Đã xoá "{cardDeleted}"')
             else:
                 print()
@@ -161,18 +158,19 @@ class Card:
                         cardDeleted = listName[n]
                         del self.ownerCardList[card]
                         break
-                self.ownerCard.writeJSonFile(self.ownerCardList)
+                self.ownerCard.wJSon(self.ownerCardList)
             elif n != 'Q':
                 print('Cú pháp không hợp lệ!')
                 time.sleep(1)
-    '''
+
 
     def details(self, number):
         os.system('cls')
-        listName = self.sortListOfName(self.ownerCardList)
+        listName = self.sortNames(self.ownerCardList)
         card = {}
-        for card in self.ownerCardList:
-            if listName == card['name']:
+        for i in self.ownerCardList:
+            if listName[number] == i['name']:
+                card = i.copy()
                 break
         print(f'{"ID:":<10}{card["id"]}')
         print(f'{"Name:":<10}{card["name"]}')
@@ -199,12 +197,11 @@ class Card:
                 except:
                     return card['stats']['mana']
 
-    '''
     def show(self):
         n = ''
         while (n != 'Q'):
             os.system('cls')
-            self.showListOfNames('owner')
+            self.showNames('owner')
             print('_' * 112)
             print('\nNhập số của thẻ để xem chi tiết')
             print('[A] Thêm    |    [D] Xoá    |    [Q]Thoát')
@@ -213,12 +210,222 @@ class Card:
                 self.add()
             elif n.isalpha() and n == 'D':
                 self.delete()
-            elif n.isdigit() and int(n) - 1 < len(card) and int(n) - 1 >= 0:
+            elif n.isdigit() and int(n) - 1 < len(self.ownerCardList) and int(n) - 1 >= 0:
                 self.details(int(n) - 1)
             elif n != 'Q':
                 print('Cú pháp không hợp lệ!')
                 time.sleep(1)
         return 'Q'
-    '''
 
-p=Card()
+class Account:
+    def __init__(self, email, password):
+        self.__email = email
+        self.__password = password
+
+    def toDict(self):
+        return {'mail': self.__email, 'pwd': self.__password}
+
+    def email(self):
+        return self.__email
+
+class AccountList:
+    def __init__(self):
+        try:
+            self.account = File('data/account.json')
+            self.accountList = self.account.rJSon()
+        except:
+            self.accountList = []
+
+    def show(self):
+        if len(self.accountList) > 0:
+            number = 1
+            for i in self.accountList:
+                print(f'{number}. {i["mail"]}')
+                number += 1
+
+    def add(self):
+        os.system('cls')
+        email = input('Email: ')
+        password = input('Mật khẩu: ')
+        account = Account(email, password)
+        self.accountList.append(account.toDict())
+        self.account.wJSon(self.accountList)
+
+    def delete(self):
+        select = None
+        while (select != 'B'):
+            os.system('cls')
+            print('DANH SÁCH TÀI KHOẢN\n')
+            if len(self.accountList) > 0:
+                j = 1
+                for i in self.accountList:
+                    print(f'{j}. {i["mail"]}')
+                    j += 1
+            else:
+                print("Không có tài khoản nào!")
+            print('\n[B] Trở lại')
+            select = input('>> Chọn: ').upper()
+            if (select.isdigit() and int(select) - 1 < len(self.accountList) and int(select) - 1 >= 0):
+                self.accountList.pop(int(select) - 1)
+                self.account.wJSon(self.accountList)
+            elif select != 'B':
+                print('Cú pháp không hợp lệ!')
+                time.sleep(1)
+            else:
+                select = 'B'
+
+    def show(self):
+        n = ''
+        while (n != 'Q'):
+            os.system('cls')
+            print('DANH SÁCH TÀI KHOẢN\n')
+            if len(self.accountList) > 0:
+                j = 1
+                for i in self.accountList:
+                    print(f'{j}. {i["mail"]}')
+                    j += 1
+                print("\n[A] Thêm    |    [D] Xoá    |    [Q] Thoát")
+                n = input('>> Chọn: ').upper()
+                if n == 'A':
+                    self.add()
+                elif n == 'D':
+                    self.delete()
+                elif n != 'Q':
+                    print('Cú pháp không hợp lệ!')
+                    time.sleep(1)
+            else:
+                print("Không có tài khoản nào, vui lòng thêm tài khoản!")
+                print("\n[A] Thêm    |    [Q] Thoát")
+                n = input('>> Chọn: ').upper()
+                if n == 'A':
+                    self.add()
+                elif n != 'Q':
+                    print('Cú pháp không hợp lệ!')
+                    time.sleep(1)
+
+
+class History:
+    def __init__(self):
+        try:
+            self.historyBattle = OpenFile('data/history.json')
+            self.history = self.historyBattle.readJSonFile()
+        except:
+            self.history = {}
+
+    def analys(self, mana):
+        select = ''
+        while (select != 'Q'):
+            os.system('cls')
+            self.topCard(mana)
+            print("Chọn đội hình để xem chi tiết\n")
+            j = 1
+            for i in self.listOfTeams[mana]:
+                print(f'[{j}] {", ".join(i)}')
+                j += 1
+            print('\n[Q] Thoát')
+            select = input('>> Chọn đội hình: ').upper()
+            if select.isdigit() and (int(select) - 1 >= 0 and int(select) - 1 < len(self.listOfTeams[mana])):
+                select = int(select) - 1
+                team = self.listOfTeams[mana][select]
+                kda = self.kda(mana, team)
+                os.system('cls')
+                print(f'Team: {", ".join(team)}')
+                print(f"\nTrong {kda[3]} trận:")
+                print(f'    Thắng: {kda[0]}')
+                print(f'    Thua: {kda[1]}')
+                print(f'    Hoà: {kda[2]}')
+                if len(self.history) > 0:
+                    if kda[3] != 0:
+                        print('\n\t\t\t\t\t\tLỊCH SỬ THUA\n')
+                        for i in range(len(self.history[mana])):
+                            if self.history[mana][i]['my_team']['team'] == team:
+                                result = self.history[mana][i]['result']
+                                if result[7:] == 'Lost':
+                                    print('\n> [', end="")
+                                    print(", ".join(self.history[mana][i]['enemy_team']['team']), end="")
+                                    print(']')
+                                    listNumCard = []
+                                    listName = numOfCard()
+                                    for k in self.history[mana][i]['enemy_team']['team']:
+                                        listNumCard.append(str(listName.get(k)))
+                                    num = ":".join(listNumCard)
+                                    print(f'--> Số thứ tự [{num}]\n')
+                                    print('-' * 120)
+                else:
+                    print('\n\t\t\t\t\t\tLỊCH SỬ THUA\n')
+                    print('\t\t\t\t\t      Không có lịch sử')
+                n = input('\n[B] Trở lại    |    [Q] Thoát\n>> Chọn: ').upper()
+                while (n != 'B' and n != 'Q'):
+                    os.system('cls')
+                    print('Cú pháp không hợp lệ!')
+                    n = input('\n[B] Trở lại    |    [Q] Thoát\n>> Chọn: ').upper()
+                if n == 'Q': select = 'Q'
+            elif select != 'Q':
+                print('Cú pháp không hợp lệ!')
+                time.sleep(1)
+
+    def kda(self, mana, team):
+        if len(self.history) > 0:
+            won = lost = drawn = match = 0
+            if self.history.get(mana) != None:
+                for i in range(len(self.history[mana])):
+                    if self.history[mana][i]['my_team']['team'] == team:
+                        if self.history[mana][i]['result'] == 'Battle Won': won += 1
+                        if self.history[mana][i]['result'] == 'Battle Lost': lost += 1
+                        if self.history[mana][i]['result'] == 'Drawn': drawn += 1
+                match = won + lost + drawn
+            return [won, lost, drawn, match]
+        else:
+            return [0, 0, 0, 0]
+
+    def writeHistory(self, driver, times):
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        rlt_soup = soup.find_all(class_="battle-log-entry")
+        for n in range(times):
+            btl_log = re.compile(r'\d+|\w+\s?\w*\s?\w*\s?\w*[^(\n)]').findall(rlt_soup[n].text)
+            me = []
+            enemy = []
+            for i in range(3, len(btl_log)):
+                if btl_log[i] != 'VS':
+                    me.append(btl_log[i])
+                else:
+                    break
+            en_name = ''
+            en_rat = ''
+            en_gui = ''
+            for j in range(len(btl_log) - 3, -1, -1):
+                if btl_log[j].isdigit():
+                    en_name = btl_log[j + 1]
+                    en_rat = btl_log[j]
+                    en_gui = btl_log[j + 2]
+                    for z in range(j + 3, len(btl_log) - 3):
+                        enemy.append(btl_log[z])
+                    break
+            result = btl_log[len(me) + 4]
+            mode = btl_log[-3]
+            mana = btl_log[len(me) + 7]
+            if mana == '0': mana = btl_log[len(me) + 9]
+            if mana == 'DEC': mana = btl_log[len(me) + 10]
+            my_team = {}
+            my_team['name'] = btl_log[1]
+            my_team['rating'] = btl_log[0]
+            my_team['guid_name'] = btl_log[2]
+            my_team['team'] = me
+            enemy_team = {}
+            enemy_team['name'] = en_name
+            enemy_team['rating'] = en_rat
+            enemy_team['guid_name'] = en_gui
+            enemy_team['team'] = enemy
+            result_ = result[:-3]
+            if result[:-3] != "Battle Lost" and result[:-3] != "Battle Won": result_ = 'Drawn'
+            match = {}
+            match['mode'] = mode[:-4]
+            match['result'] = result_
+            match['my_team'] = my_team
+            match['enemy_team'] = enemy_team
+            if self.history.get(mana) == None:
+                self.history[mana] = []
+                self.history[mana].append(match)
+            else:
+                self.history[mana].append(match)
+            self.historyBattle.writeJSonFile(self.history)
--- test.py
+++ test.py
@@ -2,23 +2,39 @@ import requests, json
 from bs4 import BeautifulSoup
 
 
-with open('source.html', encoding='utf-8') as f:
-	soup = BeautifulSoup(f, 'html.parser')
+# with open('source.html', encoding='utf-8') as f:
+# 	soup = BeautifulSoup(f, 'html.parser')
 
-card_name = soup.find_all(class_="card-name-name")
-c = []
-for i in card_name:
-	c.append(i.text)
+# card_name = soup.find_all(class_="card-name-name")
+# c = []
+# for i in card_name:
+# 	c.append(i.text)
 
-response = requests.get('https://api2.splinterlands.com/cards/get_details?')
-full = json.loads(response.text)
-k = []
-for i in c:
-	for j in full:
-		if i == j["name"]:
-			k.append(j)
-			break
-with open('t.json', 'w') as f:
-	json.dump(k, f, indent=4)
+# response = requests.get('https://api2.splinterlands.com/cards/get_details?')
+# full = json.loads(response.text)
+# k = []
+# for i in c:
+# 	for j in full:
+# 		if i == j["name"]:
+# 			k.append(j)
+# 			break
+# with open('t.json', 'w') as f:
+# 	json.dump(k, f, indent=4)
 
 
+response = requests.get('https://api2.splinterlands.com/battle/history2?player=initiate_726597')
+
+a = json.loads(response.text)
+
+
+bt = a['battles'][2]
+
+
+#v = bt.values()
+t = bt['details']['team2']['monsters'][0]['card_detail_id']
+
+print(t)
+'''
+for i in t:
+	print(f'{i} : {t[i]}')
+'''
