826fc58a507e5d4bb39a5a2e8e2b1c64b048bfc7
Kha Minh Tran <tranminhkha.1003@gmail.com>
2022-01-25 16:16:26 +0700

    add Launcher,oy

--- Launcher.py
+++ Launcher.py
@@ -1,9 +1,6 @@
-import os, time, requests
+import os, requests
 
 def download_resource():
-    print('\n\n\n\n\n\n\n')
-    print(logo)
-    print('\t\t\t\t\t           Đang tải dữ liệu...')
     response = requests.get('https://raw.githubusercontent.com/tmkha/Splint/main/splint.py')
     if response:
         with open('splint.py', mode="w", encoding="utf-8") as f:
@@ -20,7 +17,5 @@ if __name__ == "__main__":
         Launcher.menu()
     except Exception as e:
         os.system('cls')
-        print('\n\n\n\n\n\n\n')
-        print(logo)
         print('Lỗi:', e)
         os.system('pause>nul|set/p =Nhấn phím bất kì để thoát ...')
